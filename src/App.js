b3f7785787857fd3792b01a1aeebb2a65a2ee350
raulmahya <raulmahya11@gmail.com>
2023-05-07 20:08:56 +0700

    asdasd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-
+// import { Founderr } from "./components/Founder.js"
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
 <Skills />
 <Projects/>
 <Contact/>
+{/* <Founderr/> */}
 <Footer/>
     </div>
   );
